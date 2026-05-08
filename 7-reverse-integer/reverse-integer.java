5876ff834b4077fa2f02566927c7119ce39b50d4
Chaithanya C <1nc24cs034@ncetmail.com>
2026-05-08 18:52:09 +0530

    Time: 1 ms (99.97%) | Memory: 42.5 MB (79.35%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        int temp = x;
+        long rev = 0;
+        while(temp!=0){
+            int digit = temp %10;
+            rev = rev*10 + digit;
+            temp = temp /10;
+        }
+        if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int)rev;
+    }
+}
