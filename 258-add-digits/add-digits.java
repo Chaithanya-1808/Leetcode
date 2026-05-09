385dcbaa34b30a31f33881159abbc059155dd7a0
Chaithanya C <1nc24cs034@ncetmail.com>
2026-05-09 11:31:54 +0530

    Time: 1 ms (98.06%) | Memory: 42.7 MB (44.21%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int addDigits(int num) {
+         while(num >= 10){   // repeat until single digit
+            int sum = 0;
+
+            while(num > 0){
+                sum += num % 10;
+                num /= 10;
+            }
+
+            num = sum;
+        }
+
+        return num;
+    }
+}
